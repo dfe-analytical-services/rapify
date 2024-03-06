00afcb6aadc2077642bc8c8db8d4fe5e581bd0cc
Rich Bielby <richard.bielby@education.gov.uk>
2024-03-06 11:46:00 +0000

    Updated SCAP code to account for some LA changes

--- R/scap.R
+++ R/scap.R
@@ -1,4 +1,8 @@
 library(readr)
+library(dplyr)
+
+data_file <- "data/scap/school-capacity_200910-202122.csv"
+gias_file <- 'data/gias_establishment_codes.csv'
 
 urn_fix <- function(data_file, output_file, gias_file='data/gias_establishment_codes.csv'){
   data <- read_csv(data_file) %>%
@@ -20,16 +24,61 @@ urn_fix <- function(data_file, output_file, gias_file='data/gias_establishment_c
     mutate(
       school_laestab = paste0(`LA (code)`, EstablishmentNumber),
       school_urn='x',
+      URN = format(URN, scientific=FALSE),
       OpenDate = as.Date(OpenDate, format='%d-%m-%Y'),
       CloseDate = as.Date(CloseDate, format='%d-%m-%Y'),
     ) %>%
     select(URN, school_laestab, EstablishmentName, OpenDate, CloseDate)
+  data_reprocessed <- match_to_gias_data(data, gias_clean)
+  
+    unmatched_rows <- data_reprocessed %>% 
+    filter(school_urn != URN | is.na(school_urn)| school_urn=='x') %>% 
+    select(time_period, old_la_code, la_name, school_laestab, school_name) %>%
+    mutate(old_la_code = as.character(old_la_code))
+  message(paste("Found ", nrow(unmatched_rows), "unmatched rows in the reporocessed data"))
+  
+  la_evolution <- data.frame(
+    la_code_new = c("942", "943", "838", "839", "839", "839", "940", "941"),
+    la_code_previous = c("909", "909", "835", "835", "836", "837", "928", "928")
+  )
+  changed_la_lookup <- unmatched_rows %>% 
+    select(-time_period) %>%
+    distinct() %>%
+    left_join(la_evolution, by=c("old_la_code"="la_code_previous")) %>% 
+    mutate(school_laestab_new = paste0(la_code_new, stringr::str_sub(school_laestab,4,7))) %>%
+    left_join(gias_clean, by=c("school_laestab_new"="school_laestab")) %>%
+    filter(!is.na(URN)) %>%
+    select(
+      URN, school_laestab, EstablishmentName, OpenDate, CloseDate
+    ) %>%
+    distinct() %>%
+    arrange(school_laestab)
+  data_round2 <- match_to_gias_data(
+    data_reprocessed %>%
+      select(-EstablishmentName, -URN), 
+    changed_la_lookup
+    )
+  unmatched_rows_round2 <- data_round2 %>% 
+    filter(school_urn != URN | is.na(school_urn)| school_urn=='x') %>% 
+    select(time_period, old_la_code, la_name, school_laestab, school_name) %>%
+    mutate(old_la_code = as.character(old_la_code)) %>%
+    arrange(school_laestab)
+  message(paste("Found ", nrow(unmatched_rows_round2), "unmatched rows in the reporocessed data"))
+  
+  
+  message(paste("About to write out", nrow(data), "rows to",output_file))
+}
+
+
+match_to_gias_data <- function (data, gias_clean){
   data_joined <- data %>%
+    select(time_period, school_laestab, school_urn) %>%
+    distinct() %>%
     mutate(
       time_period_ref_date = as.Date(paste0(stringr::str_sub(as.character(time_period+100),1,4),"-05-01"))
-      ) %>%
+    ) %>%
     left_join(gias_clean)
-  data_joined_clean <- data_joined %>%
+  school_time_period_lookup <- data_joined %>%
     mutate(
       OpenDate = if_else(is.na(OpenDate), as.Date("1990-09-01"), OpenDate),
       CloseDate = if_else(is.na(CloseDate), Sys.Date(), CloseDate)
@@ -38,19 +87,29 @@ urn_fix <- function(data_file, output_file, gias_file='data/gias_establishment_c
       time_period_ref_date < CloseDate,
       time_period_ref_date >= OpenDate,
     )
-  message(paste("Found", nrow(data_joined_clean), " records in joined data."))
-  
-  
-  joined_duplicates <- data_joined_clean %>% 
-    summarise(count=n(), .by=c(time_period, school_laestab, school_name) ) %>%
+  message(paste("Found", nrow(school_time_period_lookup), " records in lookup table."))
+  print(school_time_period_lookup %>% filter(!is.na(URN), is.na(school_urn)))
+  lookup_duplicates <- school_time_period_lookup %>% 
+    summarise(count=n(), .by=c(time_period, school_laestab, school_urn) ) %>%
     filter(count>1) %>% arrange(school_laestab)
-  message(paste("Found", nrow(joined_duplicates), "in joined data."))
+  message(paste("Found", nrow(lookup_duplicates), " duplicates in lookup table."))
   
   message("Now rejoining this on to original data to add back in unmacthed rows")
   data_reprocessed <- data %>% 
     left_join(
-      data_joined_clean %>% select(time_period, school_laestab, URN, EstablishmentName)
+      school_time_period_lookup %>% select(time_period, school_laestab, URN, EstablishmentName)
+    ) %>%
+    mutate(
+      school_urn = case_when(
+        school_urn=='x' ~ URN,
+        is.na(school_urn) ~ URN,
+        .default = school_urn
+      ),
+      school_name = case_when(
+        !is.na(EstablishmentName) ~ EstablishmentName,
+        .default = school_name
+      )
     )
-  
-  message(paste("About to write out", nrow(data), "rows to",output_file))
+  message(paste("I've got ", nrow(data_reprocessed), "rows in the reporocessed data (compared to ",nrow(data),"in the original data"))
+  return(data_reprocessed)
 }
