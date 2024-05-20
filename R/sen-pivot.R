999c23b0ab316c61150f094712d56a76b9f34da5
Rich Bielby <richard.bielby@education.gov.uk>
2024-05-20 14:48:11 +0100

    Updates to sen code

--- R/sen-pivot.R
+++ R/sen-pivot.R
@@ -12,7 +12,10 @@ context_filters <- c("phase_type_grouping", "pupil_sen_status", "primary_need")
 pivot_sen_data <- function(file){
   data <- read_csv(file)
   pivot <- data %>% 
-    mutate(across(where(is.character), ~ if_else(is.na(.x), "", .x))) %>%
+    mutate(
+      old_la_code = as.character(old_la_code),
+      across(where(is.character), ~ if_else(is.na(.x), "", .x))
+      ) %>%
     pivot_longer(!any_of(c(standard_filters, context_filters)),
                         names_to = c("master_filter"),
                         values_to = 'value'
@@ -25,6 +28,11 @@ pivot_sen_data <- function(file){
     breakdown_topic = case_when(
       grepl("age", master_filter) ~ "Age",
       grepl("sex", master_filter) ~ "Sex",
+      grepl("FSM", master_filter) ~ "Free school meals",
+      grepl("ethnicity", master_filter) ~ "Ethnicity minor",
+      grepl("language", master_filter) ~ "First language",
+      grepl("nc_", master_filter) ~ "National curriculum year group",
+      grepl("secondary_need", master_filter) ~ "Secondary need",
       .default = 'Total'
     ),
     breakdown = case_when(
@@ -51,8 +59,11 @@ pivot_sen_data <- function(file){
   
 }
 
-sen_file <- '../../test-data/sen/sen_age_sex_.csv'
-sen_pivot <- pivot_sen_data(sen_file)
-write.csv(sen_pivot, gsub('.csv','tidy.csv', sen_file), row.names=FALSE)
+age_sex_file <- '../../test-data/sen/sen_age_sex_.csv'
+age_sex_pivot <- pivot_sen_data(age_sex_file)
+write.csv(age_sex_pivot, gsub('.csv','tidy.csv', age_sex_file), row.names=FALSE)
 
+fsm_ethnicity_lang_file <- '../../test-data/sen/sen_fsm_ethnicity_language_.csv'
+fsm_ethnicity_lang_pivot <- pivot_sen_data(fsm_ethnicity_lang_file)
+write.csv(fsm_ethnicity_lang_pivot, gsub('.csv','tidy.csv', fsm_ethnicity_lang_file), row.names=FALSE)
 
