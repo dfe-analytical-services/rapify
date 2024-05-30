068dd8406b9da03d02c99e3dcc444e742174b4cd
Rich Bielby <richard.bielby@education.gov.uk>
2024-05-30 10:43:14 +0100

    Tidying code

--- R/long-term-movement.R
+++ R/long-term-movement.R
@@ -2,21 +2,43 @@ library("readr")
 library("dplyr")
 library("dfeR")
 
-geographic_lookup <- read_csv()
+la_code_lookup_raw <- read_csv("https://raw.githubusercontent.com/dfe-analytical-services/dfe-published-data-qa/master/data/ward_lad_la_pcon_hierarchy.csv") 
+
+la_code_lookup <- la_code_lookup_raw %>% 
+  filter(most_recent_year_included>=2023) %>%
+  select(new_la_code, la_name) %>% distinct()
 
 file <- "../../test-data/leo_graduate_outcomes/LEO_provider_full_cycle_movement_data_2021.csv"
 
 movement_data <- read_csv(file)
 
-movement_data %>% 
+ees_version <- movement_data %>% 
   mutate(
    time_period = gsub("/20", "", tax_year),
    time_identifier = "Tax year",
    geographic_level = "Provider",
    ukprn = paste(ukprn)
   ) %>%
+  left_join(la_code_lookup) %>%
+  mutate(
+    country_name = case_when(
+      grepl("S", new_la_code) ~ 'Scotland',
+      grepl("W", new_la_code) ~ 'Wales',
+      grepl("N", new_la_code) ~ 'Northern Ireland',
+      .default = 'England'
+    ),
+    country_code = case_when(
+      grepl("S", new_la_code) ~ 'S92000003',
+      grepl("W", new_la_code) ~ 'W92000004',
+      grepl("N", new_la_code) ~ 'N92000002',
+      .default = 'E92000001'
+    )
+    
+  )
+
+
   select(
-    tim_period, time_identifier, 
+    time_period, time_identifier, 
     geographic_level, country_name, country_code, 
     region_name, region_code, la_name, new_la_code, old_la_code,
     provider_name, ukprn,
