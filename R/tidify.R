efadc8dda96bbed253595a2d2e0c205a41a67a71
Rich Bielby <richard.bielby@education.gov.uk>
2024-06-21 11:54:03 +0100

    Updated tidify function to take first underscore rather than last

--- R/tidify.R
+++ R/tidify.R
@@ -7,7 +7,7 @@ pivot_filter_indicator <- function(df, filter_name = "grade",
                                    )) {
   dftidied <- df %>% pivot_longer(!any_of(c(keep_filters)),
     names_to = c(".value", filter_name),
-    names_pattern = "(.*)_(.*)"
+    names_pattern = "(.*?)_(.*)"
   )
   return(dftidied)
 }
