c6b1d79af8ee3507394b3d20dc18b6b1ea1d6f2d
Rich Bielby <richard.bielby@education.gov.uk>
2024-06-28 13:31:58 +0100

    Adding in demo mutate code for ect data

--- teacher-learning-development.R
+++ teacher-learning-development.R
@@ -3,6 +3,7 @@ source('R/tidify.R')
 
 teacher_leader_development_ecf_2022_23 <- read_csv("data/teacher_leader_development_ecf_2022-23.csv")
 tidydata <-pivot_filter_indicator(teacher_leader_development_ecf_2022_23 ,
+                                  filter_name = "type_ects_training",
                                   keep_filters = c("time_period"   ,                                      "time_identifier"  ,                                  
                                                    "geographic_level"     ,                               "country_code"  ,                                     
                                                    "country_name" ,                                       "region_code" ,                                       
@@ -10,8 +11,29 @@ tidydata <-pivot_filter_indicator(teacher_leader_development_ecf_2022_23 ,
                                               "old_la_code"  ,                                       "new_la_code",                                        
                                             "breakdown"  ,                                         "group_name"  ) )
 
+# Functions used
+# mutate() - creates or changes a column
+# case_when() - change an entry in a cell based on a set of criteria
+# grepl() - return true if a sub-string is found in a string
+# %>% - pipe (send output from one function as input for the next)
+cleaned <- tidydata %>% mutate(
+  dummy_column = 23846,
+  filter_ecfs = case_when(
+    type_ects_training == "swfc_ects" ~ "Early career teachers in School Workforce Census",
+    type_ects_training == "mentors_trained_count_provider_led" ~ "Mentors trained for provider-led ECF-based induction",
+    .default = type_ects_training
+  ),
+  school_provider_led = case_when(
+    grepl("provider_led", type_ects_training) ~ "Provider led",
+    .default = 'Total'
+  )
+  )
+View(cleaned)
+
+
+
 View(tidydata)
-print(tidydata$grade %>% unique())
+print(tidydata$type_ects_training %>% unique())
 
 # Read in meta file
 ecf_meta <- read_csv("data/teacher_leader_development_ecf_2022-23.meta.csv")
