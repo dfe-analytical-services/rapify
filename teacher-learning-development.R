622da9c910098dc2c7026c8c8295c1e7e7ab1d6c
Rich Bielby <richard.bielby@education.gov.uk>
2024-06-28 12:57:05 +0100

    Added the right meta data file for ECTs now

--- teacher-learning-development.R
+++ teacher-learning-development.R
@@ -1,6 +1,7 @@
 library(readr)
-teacher_leader_development_ecf_2022_23 <- read_csv("data/teacher_leader_development_ecf_2022-23.csv")
 source('R/tidify.R') 
+
+teacher_leader_development_ecf_2022_23 <- read_csv("data/teacher_leader_development_ecf_2022-23.csv")
 tidydata <-pivot_filter_indicator(teacher_leader_development_ecf_2022_23 ,
                                   keep_filters = c("time_period"   ,                                      "time_identifier"  ,                                  
                                                    "geographic_level"     ,                               "country_code"  ,                                     
@@ -8,3 +9,10 @@ tidydata <-pivot_filter_indicator(teacher_leader_development_ecf_2022_23 ,
                                                  "region_name"  ,                                       "la_name"   ,                                         
                                               "old_la_code"  ,                                       "new_la_code",                                        
                                             "breakdown"  ,                                         "group_name"  ) )
+
+View(tidydata)
+print(tidydata$grade %>% unique())
+
+# Read in meta file
+ecf_meta <- read_csv("data/teacher_leader_development_ecf_2022-23.meta.csv")
+View(ecf_meta)
