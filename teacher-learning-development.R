942f7d5eea8dd5c4cf406d6ffdf591a8c01a1665
LO <Mbene.LO@EDUCATION.GOV.UK>
2024-07-01 09:29:34 +0100

    commiting

--- teacher-learning-development.R
+++ teacher-learning-development.R
@@ -17,18 +17,25 @@ tidydata <-pivot_filter_indicator(teacher_leader_development_ecf_2022_23 ,
 # grepl() - return true if a sub-string is found in a string
 # %>% - pipe (send output from one function as input for the next)
 cleaned <- tidydata %>% mutate(
-  dummy_column = 23846,
-  filter_ecfs = case_when(
-    type_ects_training == "swfc_ects" ~ "Early career teachers in School Workforce Census",
+dummy_column = 23846 ,
+  filter_ecfs = case_when( 
+    type_ects_training == "swfc_ects" ~ "Early career teachers in School Workforce Census", 
+    type_ects_training == "swfc_ects_school_led" ~ "Percentage of ECTs who started school-led ECF-based induction",
+    type_ects_training == "swfc_ects_pending_confirmation_provider_led" ~ "Percentage of ECTs who started provider-led ECF-based",
+    type_ects_training == "swfc_ects_provider_led" ~ "Percentage of ECTs who started provider-led ECF-based induction",
+    type_ects_training == "ects_count_provider_led" ~ "Total number of ECTs who started provider-led ECF-based induction",
     type_ects_training == "mentors_trained_count_provider_led" ~ "Mentors trained for provider-led ECF-based induction",
     .default = type_ects_training
   ),
   school_provider_led = case_when(
     grepl("provider_led", type_ects_training) ~ "Provider led",
+    .default = 'Total',
+    grepl("Total", type_ects_training) ~ "School led",
     .default = 'Total'
   )
-  )
+)
 View(cleaned)
+
 
 
 
