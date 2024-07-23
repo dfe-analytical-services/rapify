1510e47a9ed3c0242f796a0bdd2c9b427b0447bf
BIELBY <Richard.BIELBY@education.gov.uk>
2024-07-23 16:11:24 +0100

    Corrected assumed fields in the KS4 attainment code

--- R/cscp-tidify.r
+++ R/cscp-tidify.r
@@ -229,19 +229,34 @@ ks4_attainment8 <- function(){
     pivot_longer(-all_of(c("LEA", "ESTAB", "URN", "SCHNAME")), names_to = "master_filter")  %>%
     mutate(
       metric = case_when(
-        grepl("ATT8SCR", master_filter) ~ "attainment_score",
-        grepl("TOTATT8", master_filter) ~ "pupil_count",
-        grepl("DIFFN", master_filter) ~ "diffn",
+        grepl("ATT8SCR", master_filter) ~ "attainment_score_average",
+        grepl("TOTATT8", master_filter) ~ "attainment_score_total",
         .default = "unknown"
       ),
-      sex = case_when(
-        grepl("BOYS", master_filter) ~ "Male",
-        grepl("GIRLS", master_filter) ~ "Female",
+      breakdown_topic = case_when(
+        grepl("BOYS", master_filter) ~ "Sex",
+        grepl("GIRLS", master_filter) ~ "Sex",
+        grepl("_NFSM", master_filter) ~ "Free school meals status",
+        grepl("_FSM", master_filter) ~ "Free school meals status",
+        grepl("NMOB", master_filter) ~ "Mobility status",
+        grepl("EAL", master_filter) ~ "Language status",
+        grepl("_LO", master_filter) ~ "Prior attainment",
+        grepl("_MID", master_filter) ~ "Prior attainment",
+        grepl("_HI", master_filter) ~ "Prior attainment",
+        grepl("DIFFN", master_filter) ~ "Prior attainment",
         .default = "All pupils"
       ),
-      free_school_meal_status = case_when(
+      breakdown = case_when(
+        grepl("BOYS", master_filter) ~ "Male",
+        grepl("GIRLS", master_filter) ~ "Female",
         grepl("_NFSM", master_filter) ~ "Not free school meals",
         grepl("_FSM", master_filter) ~ "Free school meals",
+        grepl("NMOB", master_filter) ~ "Non-mobile",
+        grepl("EAL", master_filter) ~ "English as another language",
+        grepl("_LO", master_filter) ~ "Low",
+        grepl("_MID", master_filter) ~ "Mid",
+        grepl("_HI", master_filter) ~ "High",
+        grepl("DIFFN", master_filter) ~ "Difference between High and Low",
         .default = "All pupils"
       ),
       time_period = case_when(
@@ -249,28 +264,14 @@ ks4_attainment8 <- function(){
         grepl("_21", master_filter) ~ "202021",
         .default = "202223"
       ),
-      mobility_status = case_when(
-        grepl("NMOB", master_filter) ~ "Non-mobile",
-        .default = "All pupils"
-      ),
-      eal_status = case_when(
-        grepl("EAL", master_filter) ~ "English as another language",
-        .default = "All pupils"
-      ),
       attainment_area = case_when(
-        grepl("OPENNG", master_filter) ~ "Open NG",
-        grepl("OPENG", master_filter) ~ "Open G",
+        grepl("OPENNG", master_filter) ~ "Open non-GCSE",
+        grepl("OPENG", master_filter) ~ "Open GCSE",
         grepl("OPEN", master_filter) ~ "Open",
         grepl("ENG", master_filter) ~ "English",
         grepl("MAT", master_filter) ~ "Maths",
         grepl("EBAC", master_filter) ~ "EBACC pupils",
         .default = "All subjects"
-      ),
-      prior_attainment = case_when(
-        grepl("_LO", master_filter) ~ "Low",
-        grepl("_MID", master_filter) ~ "Mid",
-        grepl("_HI", master_filter) ~ "High",
-        .default = "All pupils"
       ),
       value = case_when(
         value == "NE" ~ "z",
@@ -283,19 +284,6 @@ ks4_attainment8 <- function(){
       )
     )
 
-  ks4_a8 %>% filter(
-    URN=='100003', 
-    sex=='All pupils', 
-    time_period=='202223', 
-    free_school_meal_status == 'All pupils', 
-    mobility_status == 'All pupils', 
-    eal_status=='All pupils', 
-    attainment_area=='English', 
-    prior_attainment=='All pupils'
-    ) %>%
-    arrange(metric, master_filter) %>%
-    print(n=50)
-  
   ks4_a8_tidied <- ks4_a8 %>%
     select(-master_filter) %>%
     pivot_wider(names_from = metric, values_from = value) %>%
@@ -304,7 +292,7 @@ ks4_attainment8 <- function(){
   
   ks4_a8_ees <- ks4_a8_tidied %>%
     mutate(
-      across(c("pupil_count", "attainment_score", "diffn"), ~  if_else(is.na(.), 'z', .)),
+      across(c("attainment_score_total", "attainment_score_average"), ~  if_else(is.na(.), 'z', .)),
       school_laestab = case_when(
         is.na(ESTAB) ~ "",
         ESTAB == "NA" ~ "",
@@ -341,17 +329,16 @@ ks4_attainment8 <- function(){
       school_laestab,
       school_urn,
       school_name,
-      sex,
-      free_school_meal_status,
-      eal_status,
-      mobility_status,
-      prior_attainment,
+      breakdown_topic,
+      breakdown, 
       attainment_area,
-      pupil_count,
-      attainment_score,
-      diffn
+      attainment_score_total,
+      attainment_score_average
     ) %>%
-    arrange(-as.numeric(time_period), old_la_code, school_laestab, sex, free_school_meal_status, eal_status, mobility_status, attainment_area)
+    arrange(
+      -as.numeric(time_period), old_la_code, school_laestab, 
+      breakdown_topic, breakdown, attainment_area
+      )
 
   write.csv(
     ks4_a8_ees %>%
@@ -376,10 +363,10 @@ ks4_attainment8 <- function(){
       filter_grouping_column = ""
     )
   
-  ks4_a8_meta_data$col_type[7:8] <- "Indicator"
-  ks4_a8_meta_data$indicator_dp[7] <- "0"
-  ks4_a8_meta_data$indicator_dp[8] <- "2"
-  
+  ks4_a8_meta_data$col_type[4:5] <- "Indicator"
+  ks4_a8_meta_data$indicator_dp[4] <- "1"
+  ks4_a8_meta_data$indicator_dp[5] <- "2"
+
   print(ks4_a8_meta_data)
   write.csv(ks4_a8_meta_data, paste0(data_folder, "cscp_ks4_attainment8_202223.meta.csv"), row.names = FALSE)
   
