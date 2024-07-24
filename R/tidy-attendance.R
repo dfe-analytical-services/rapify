2f0447118ff82d443978b9da80bd0940b4938ad7
Rich Bielby <Richard.BIELBY@education.gov.uk>
2024-07-24 17:25:35 +0100

    Attendance API data - bits of clean-up around time periods

--- R/tidy-attendance.R
+++ R/tidy-attendance.R
@@ -4,6 +4,8 @@ library(dplyr)
 library(tidyr)
 library(stringr)
 
+source("R/ees-functions.R")
+
 home_dir <- Sys.getenv("HOME") |> strsplit("\\\\")
 
 data_folder <- paste0(paste0(home_dir[[1]][1:3], collapse = "/"), "/test-data/api-attendance/")
@@ -17,27 +19,45 @@ primary_filters <- c(
   "establishment_phase"
 )
 
+initial_clean <- function(attendance_data){
+  attendance_cleaned <- attendance_data %>%
+    rename(
+      time_frame = breakdown,
+      establishment_phase = school_type
+    ) %>%
+    mutate(
+      time_identifier = paste("Week", time_identifier),
+      time_frame = gsub("YTD", "Year to date", time_frame),
+      day_number = if_else(time_frame == "Weekly", "Total", as.character(day_number))
+    ) %>%
+    rename_all( ~ stringr::str_replace_all(., 'auth_', 'authorised_'))
+  time_lookup <- attendance_cleaned %>% 
+    select(attendance_date, time_period, time_identifier, week_commencing) %>% 
+    distinct() %>% 
+    arrange(week_commencing, time_period, time_identifier) %>% 
+    filter(
+      !is.na(week_commencing), 
+      time_period==week_commencing %>% lubridate::year()
+      )
+  attendance_cleaned <- attendance_cleaned %>% 
+    select(-week_commencing, -time_period, -time_identifier) %>%
+    left_join(time_lookup)
+  attendance_cleaned
+}
+
 read_attendance <- function() {
   url <- "https://raw.githubusercontent.com/dfe-analytical-services/attendance-data-dashboard/main/data/attendance_data_dashboard.csv"
   data_file <- "attendance_data_dashboard.csv"
   if (!file.exists(paste0(data_folder, data_file))) {
     message(paste0(data_folder, data_file, "\n not found. Downloading from repository."))
     att_wide <- read_csv(url) %>%
-      rename(
-        time_frame = breakdown,
-        establishment_phase = school_type
-        ) %>%
-      mutate(time_identifier = paste("Week", time_identifier))
+      initial_clean()
     att_wide |> write_csv(paste0(data_folder, data_file))
   } else {
     message(paste0(data_folder, data_file, " found. Readng in from file
                    ."))
     att_wide <- read_csv(paste0(data_folder, data_file)) %>%
-      rename(
-        time_frame = breakdown,
-        establishment_phase = school_type
-        ) %>%
-      mutate(time_identifier = paste("Week", time_identifier))
+      initial_clean()
   }
   att_wide
 }
@@ -58,12 +78,16 @@ create_reasons_tidy <- function() {
     mutate(
       reason = str_to_sentence(reason %>% gsub("_", " ", .)),
       reason = gsub("aea", "AEA", reason),
-      across(region_code:old_la_code, ~ if_else(is.na(.), "", as.character(.)))
+      across(region_code:old_la_code, ~ if_else(is.na(.), "", as.character(.))),
+      pupil_count = if_else(is.na(pupil_count), "x", as.character(pupil_count))
     ) %>%
     select(all_of(c(primary_filters, "reason", "pupil_count")))
   write_csv(reason_tidy, paste0(data_folder, "attendance_reasons.csv"))
   reason_meta <- meta_template(reason_tidy) %>% 
-    filter(!(col_name %in% c("attendance_date", "week_commencing")))
+    filter(!(col_name %in% c("attendance_date", "week_commencing", "time_frame"))) %>%
+    mutate(
+      filter_grouping_column = if_else(col_name == "day_number", "time_frame", "")
+      )
   write_csv(reason_meta, paste0(data_folder, "attendance_reasons.meta.csv"))
   reason_tidy
 }
