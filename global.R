be247c96585c0426c9c18a18033ba2431b1ec305
SURTEES <Tia-Mae.SURTEES@EDUCATION.GOV.UK>
2025-07-09 10:28:21 +0100

    Updated for week 26 publication. global.R file updated to source the functions script and run each function.

--- global.R
+++ global.R
@@ -1,3 +1,13 @@
-library(dplyr)
-library(styler)
-library(tidyr)
+# Run script
+
+# First change the dates in attendance-tidify.R to reflect the current week number
+# If the previous release number was not 2 weeks ago, i.e. it was 1 or 3 weeks ago, then change the number in the PA function
+
+# Source script with functions in
+source("attendance-tidify.R")
+
+# Run functions
+create_reasons_tidy()
+create_persistent_absence_tidy()
+create_school_returns_tidy()
+create_enrol_tidy()
